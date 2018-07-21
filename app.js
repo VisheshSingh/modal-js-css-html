3ece943c3508b07fdb1f5bad857c4d67caa016f4
Vishesh <visheshsingh16@gmail.com>
2018-07-21 18:06:40 -0500

    event listeners for closing modal

--- app.js
+++ app.js
@@ -2,7 +2,16 @@ var button = document.querySelectorAll("button");
 var backdrop = document.querySelector(".backdrop");
 var modal = document.querySelector(".modal");
 
+function closeModal() {
+  modal.style.display = "none";
+  backdrop.style.display = "none";
+}
+
 button[2].addEventListener("click", function() {
   modal.style.display = "block";
   backdrop.style.display = "block";
 });
+
+button[0].addEventListener("click", closeModal);
+
+button[1].addEventListener("click", closeModal);
