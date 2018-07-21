159031ebacba54cd44a7ae33fa2ab8c78d2bd1f4
Vishesh <visheshsingh16@gmail.com>
2018-07-21 18:17:38 -0500

    updating value in modal

--- app.js
+++ app.js
@@ -2,16 +2,45 @@ var button = document.querySelectorAll("button");
 var backdrop = document.querySelector(".backdrop");
 var modal = document.querySelector(".modal");
 
+var quote = "A modal component";
+var outputPara = document.querySelector("#quote");
+outputPara.textContent = quote;
+
+var textEdit = document.querySelector(".modal textarea");
+var editedQuote = "";
+
+function updateParagraph() {
+  outputPara.textContent = quote;
+}
+
 function closeModal() {
   modal.style.display = "none";
   backdrop.style.display = "none";
 }
 
+updateParagraph();
+
 button[2].addEventListener("click", function() {
   modal.style.display = "block";
   backdrop.style.display = "block";
+  editedQuote = quote;
+  textEdit.value = quote;
+});
+
+backdrop.addEventListener("click", function() {
+  closeModal();
 });
 
 button[0].addEventListener("click", closeModal);
 
-button[1].addEventListener("click", closeModal);
+button[1].addEventListener("click", function() {
+  closeModal();
+  if (editedQuote.trim().length > 0) {
+    quote = editedQuote;
+    updateParagraph();
+  }
+});
+
+textEdit.addEventListener("input", function() {
+  editedQuote = textEdit.value;
+});
